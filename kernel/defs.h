bcaeec459361549fb914e88e965e1fee852e7830
Gravity97 <1842963193@qq.com>
2023-08-17 17:20:00 +0000

    finish lab8

--- kernel/defs.h
+++ kernel/defs.h
@@ -67,6 +67,8 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+int             steal(int);
+void            wlalloc(int, int);
 
 // log.c
 void            initlog(int, struct superblock*);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,8 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define MAXST 64
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -21,12 +23,17 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  uint64 wl[MAXST];        //waiting list
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++){
+    char lockname[16] = {0};
+    snprintf(lockname, 16, "kmemlock%d", i + 1);
+    initlock(&kmem[i].lock, lockname);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +63,14 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();  //shutdown interrupt
+  int id = cpuid();
+  pop_off();   //restart interrupt
+
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,13 +81,69 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();  //shutdown interrupt
+  int id = cpuid();
+  
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+  if(r){
+    kmem[id].freelist = r->next;
+    release(&kmem[id].lock);
+  }
+  else{
+    // no free mem, need to steal.
+    release(&kmem[id].lock);
+
+    int count = steal(id);
+    if(count > 0){
+      acquire(&kmem[id].lock);
+      wlalloc(id, count);
+      r = kmem[id].freelist;
+      kmem[id].freelist = r->next;
+      release(&kmem[id].lock);
+    }
+  }
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+
+  pop_off();   //restart interrupt
   return (void*)r;
+}
+
+int 
+steal(int id)
+{
+  memset(kmem[id].wl, 0, MAXST);
+
+  int count = 0;
+  for (int i = 0; i < NCPU; i++){
+    if(i == id)
+      continue;
+
+    acquire(&kmem[i].lock);
+    while(kmem[i].freelist && (MAXST - count) > 0){
+      kmem[id].wl[count++] = (uint64)kmem[i].freelist;
+      kmem[i].freelist = kmem[i].freelist->next;
+    }
+    release(&kmem[i].lock);
+
+    if(count == MAXST)
+      break;
+  }
+
+  return count;
+}
+
+void 
+wlalloc(int id, int sz)
+{
+  for (int i = 0; i < sz && i < MAXST; i++){
+    if(!kmem[id].wl[i])
+      break;
+
+    struct run* p = (struct run*)kmem[id].wl[i];
+    (p)->next = kmem[id].freelist;
+    kmem[id].freelist = p;
+  }
 }
